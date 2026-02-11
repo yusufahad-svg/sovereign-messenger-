a672d9a4770b4c30f881c1c3b816a284c57ea274
Fz <macbook@MacBookPro.lan>
2026-02-11 11:47:52 +0000

    Fix logic crash and restore website

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.sovereign.messenger',
   appName: 'SovereignMessenger',
-  webDir: 'tip-jar-pwa/dist', // Updated path
-  server: {
-    url: 'https://web-production-f59a4.up.railway.app/',
-    cleartext: true
-  }
+  webDir: 'build'
 };
 
 export default config;
